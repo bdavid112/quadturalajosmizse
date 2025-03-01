3799d52bca90cd101c29f7ba6ebede567d9b7301
bdavid112 <dawid8688@gmail.com>
2025-03-01 17:23:08 +0100

    Revert "Variable setup"
    This reverts commit 45611fbb236f0b7855e366aaf655e3e4e00b1a4f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
-import './styles/global.scss'
-import './styles/grid.scss'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <h1 className="col-12 hero-text">Rólunk</h1>
-        </div>
-        <div className="row">
-          <p className="col-12">
-            Tapasztalat és szenvedély a vezetett túrákban – ismerj meg minket!
-          </p>
-        </div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
